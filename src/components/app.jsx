4d1c3beff588b3c913119e2bb5d6cd7825c15d88
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-09 12:58:35 -0400

    install react and react-dom, first try of react code

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {WelcomeScreen} from "./welcome-screen.jsx";
+
+export const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorCount} = props;
+  return <WelcomeScreen
+    errorCount={errorCount}
+  />;
+};
