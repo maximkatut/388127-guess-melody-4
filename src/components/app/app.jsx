6ce35c9f16caa1177ea438a333433d8e00c479fa
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-12 01:32:07 -0400

    install prop-types and setup validation for components

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+const App = (props) => {
+  const {errorCount} = props;
+  return <WelcomeScreen
+    errorCount={errorCount}
+  />;
+};
+
+App.propTypes = {
+  errorCount: PropTypes.number.isRequired
+};
+
+export default App;
