5a812cd39afc8e5ecf759c0b26686e15af4d5aa7
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-14 20:30:07 -0400

    add on button click handler

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import PropTypes from 'prop-types';
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
+const welcomeButtonHandler = () => {};
+
 const App = (props) => {
   const {errorCount} = props;
   return <WelcomeScreen
     errorCount={errorCount}
+    onWelcomeButtonClick = {welcomeButtonHandler}
   />;
 };
 
