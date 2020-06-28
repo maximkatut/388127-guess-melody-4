237f87198053b35a255970a05c48d363c18d2c10
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-28 15:27:46 -0400

    create Mistakes component

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,9 +1,11 @@
 import {GameType} from '../../const.js';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import React from 'react';
+import Mistakes from '../mistakes/mistakes.jsx';
 
 const GameScreen = (props) => {
-  const {type, children} = props;
+  const {type, children, mistakes} = props;
 
   return (
     <section className={`game game--${type}`}>
@@ -15,11 +17,9 @@ const GameScreen = (props) => {
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx={390} cy={390} r={370} style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
         </svg>
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        <Mistakes
+          count={mistakes}
+        />
       </header>
       {children}
     </section>
@@ -31,7 +31,14 @@ GameScreen.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
-  type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired
+  type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  mistakes: PropTypes.number.isRequired,
 };
 
-export default GameScreen;
+const mapStateToProps = (state) => ({
+  mistakes: state.mistakes
+});
+
+export {GameScreen};
+
+export default connect(mapStateToProps)(GameScreen);
