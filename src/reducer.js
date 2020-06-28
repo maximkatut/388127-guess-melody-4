fb200f11864524c4194b974384e1dd071c31310f
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-28 15:54:55 -0400

    create action for mistakes, check is answer correct, update test

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 import {extend} from './utils';
 import questions from './mocks/questions.js';
+import {GameType} from './const';
 
 const initialState = {
   step: -1,
@@ -14,11 +15,39 @@ const ActionType = {
   INCREMENT_STEP: `INCREMENT_STEP`
 };
 
+const isArtistAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.artist === question.song.artist;
+};
+
+const isGenreAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.every((answer, index) => {
+    return answer === (question.answers[index].genre === question.genre);
+  });
+};
+
 const ActionCreator = {
   incrementStep: () => ({
     type: ActionType.INCREMENT_STEP,
     payload: 1,
-  })
+  }),
+
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1
+    };
+  }
 };
 
 const reducer = (state = initialState, action) => {
